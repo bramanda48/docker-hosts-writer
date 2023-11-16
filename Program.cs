4a011ff664bd18093f8d55ee5fcca2fcc88097f3
Bramanda Suwandi <bramanda48@gmail.com>
2023-11-17 00:10:02 +0700

    feat(log): initialize windows event log

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 using docker_hosts_writer;
+using System.Diagnostics;
+
+
+// Windows event log variable
+const string windowsEventSourceName = "Docker Hosts Writer (Worker)";
+const string windowsEventLogName = "Docker Hosts Writer";
+
+if (OperatingSystem.IsWindows())
+{
+    // Create event log
+    if (!EventLog.SourceExists(windowsEventSourceName))
+        EventLog.CreateEventSource(new EventSourceCreationData(windowsEventSourceName, windowsEventLogName));
+}
 
 IHost host = Host.CreateDefaultBuilder(args)
     .UseWindowsService(options =>
@@ -6,6 +19,19 @@ IHost host = Host.CreateDefaultBuilder(args)
         options.ServiceName = "Docker Hosts Writer";
     })
     .UseSystemd()
+    .ConfigureLogging(log =>
+    {
+        log.AddSimpleConsole(options =>
+        {
+            options.SingleLine = true;
+            options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+        });
+        log.AddEventLog(options =>
+        {
+            options.SourceName = windowsEventSourceName;
+            options.LogName = windowsEventLogName;
+        });
+    })
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
